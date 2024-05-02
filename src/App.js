8a5e146a485bdba85bdbfbe6424edf8f79f853a2
Evgeniia <kuznetsovaevgeni@gmail.com>
2024-05-02 14:36:33 +0300

    added cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { CardContainer } from './components/CardContainer/CardContainer';
 
 function App() {
   return (
     <>
-    
+    <CardContainer/>
     </>
   );
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import s from './Card.module.css'
+
+export const Card = ({src, alt, animalType, animalName, gender, color, age, price}) => {
+    return <div className={s.card}>
+        <img src={src} alt={alt} className={s.photo}/>
+        <div className={s.about}>
+            <h2 className={s.title}>{animalName}</h2>
+            <p className={s.descr}>Вид животного: {animalType}</p>
+            <p className={s.descr}>Пол: {gender}</p>
+            <p className={s.descr}>Окрас: {color}</p>
+            <p className={s.descr}>Возраст: {age}</p>
+            <p className={s.price}>Цена: {price}</p>
+        </div>
+    </div>
+}
